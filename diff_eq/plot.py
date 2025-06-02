277d35e457197c11f8be970baa617f6e980a1387
ilya <ilya@ilya.ilya.org>
2025-06-02 10:51:35 +0300

    delete comments from plot.py

--- diff_eq/plot.py
+++ diff_eq/plot.py
@@ -5,27 +5,23 @@ def read_data_from_file(filename):
     with open(filename, "r") as file:
         for line in file:
             line = line.strip()
-            if line:  # Проверка на пустую строку
+            if line:
                 name, x, y = line.split(',')
                 x = int(x)
                 y = float(y)
                 if name not in data:
-                    data[name] = ([], [])  # Инициализация списков для x и y
-                data[name][0].append(x)  # Добавление x
-                data[name][1].append(y)  # Добавление y
+                    data[name] = ([], [])
+                data[name][0].append(x)
+                data[name][1].append(y)
     return data
 
-# Чтение данных из двух файлов
 data1 = read_data_from_file("results_vector.txt")
 data2 = read_data_from_file("results_scalar.txt")
 
-# Уникальные названия методов
 methods = set(data1.keys()).union(set(data2.keys()))
 
-# Создание подграфиков
 fig, axs = plt.subplots(len(methods), 1, figsize=(10, 15))
 
-# Построение графиков для каждого метода
 for i, method in enumerate(methods):
     if method in data1 and method in data2:
         x_values1, y_values1 = data1[method]
@@ -34,19 +30,16 @@ for i, method in enumerate(methods):
         axs[i].plot(x_values1, y_values1, marker='o', label=f"{method} (File 1)")
         axs[i].plot(x_values2, y_values2, marker='x', label=f"{method} (File 2)")
         
-        axs[i].set_title(f"Сравнение метода {method}", fontsize=14)  # Уменьшение размера шрифта
+        axs[i].set_title(f"Сравнение метода {method}", fontsize=14)
         axs[i].set_xlabel("Количество переменных (N)", fontsize=12)
         axs[i].set_ylabel("Время выполнения (секунды)", fontsize=12)
         axs[i].legend()
         axs[i].grid()
-        axs[i].set_xscale('log')  # Логарифмическая шкала по оси x
-        axs[i].set_yscale('linear')  # Линейная шкала по оси y
+        axs[i].set_xscale('log')
+        axs[i].set_yscale('linear')
 
-# Настройка общего оформления
-plt.tight_layout(pad=3.0)  # Увеличение отступов между подграфиками
+plt.tight_layout(pad=3.0)
 
-# Сохранение графиков в файл
 plt.savefig("results_comparison_plots.png")
 
-# Отображение графиков
 plt.show()
